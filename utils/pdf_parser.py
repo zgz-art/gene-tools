93a38712975e21e11e0de55cc0eb28bfbfd230a9
zgz-art <595187937@qq.com>
2026-05-15 10:25:21 +0800

    Delete utils directory

--- utils/pdf_parser.py
+++
@@ -1,9 +0,0 @@
-import pypdf
-
-def extract_text_from_pdf(pdf_file) -> str:
-    """从上传的 PDF 文件中提取纯文本"""
-    reader = pypdf.PdfReader(pdf_file)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-    return text
